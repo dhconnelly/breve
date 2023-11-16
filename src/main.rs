dadedb28bd034e355746d794b0ee58e14c68689f
Daniel Connelly <dhconnelly@gmail.com>
2023-11-16 20:50:15 +0100

    add a database

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use axum::{routing::get, Router};
+use sqlx::PgPool;
+
+#[derive(Clone)]
+struct AppState {
+    pool: PgPool,
+}
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
-
+async fn main(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+) -> shuttle_axum::ShuttleAxum {
+    let state = AppState { pool };
+    let router = Router::new().route("/", get(hello_world)).with_state(state);
     Ok(router.into())
 }
