9b227f1ec8b093648220cc7dfe87e0ca9fa2b426
Daniel Connelly <dhconnelly@gmail.com>
2023-11-18 21:13:02 +0100

    create html

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use axum::{
-    extract::{Path, State},
+    extract::{Form, Path, State},
     http::StatusCode,
-    response::Redirect,
+    response::{Html, Redirect},
     routing::{get, post},
     Router,
 };
 use nanoid;
+use rust_embed::RustEmbed;
+use serde::Deserialize;
 use shuttle_runtime;
 use sqlx::{Error, PgPool};
 use url::Url;
@@ -13,6 +15,24 @@ use url::Url;
 #[derive(Clone)]
 struct AppState {
     pool: PgPool,
+}
+
+#[derive(RustEmbed)]
+#[folder = "static/"]
+struct Assets;
+
+#[derive(Deserialize)]
+struct ShortenRequest {
+    pub url: String,
+}
+
+async fn index() -> Result<Html<String>, StatusCode> {
+    let file =
+        Assets::get("index.html").ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
+    let data = file.data.into_owned();
+    let index = String::from_utf8(data)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(Html(index))
 }
 
 async fn redirect(
@@ -32,9 +52,9 @@ async fn redirect(
 
 async fn shorten(
     State(state): State<AppState>,
-    url: String,
+    Form(form): Form<ShortenRequest>,
 ) -> Result<(StatusCode, String), StatusCode> {
-    let url = Url::parse(&url).map_err(|_| StatusCode::BAD_REQUEST)?;
+    let url = Url::parse(&form.url).map_err(|_| StatusCode::BAD_REQUEST)?;
     let id = nanoid::nanoid!(21);
     sqlx::query("INSERT INTO urls(id, url) VALUES ($1, $2)")
         .bind(&id)
@@ -56,6 +76,7 @@ async fn main(
 
     let state = AppState { pool };
     let router = Router::new()
+        .route("/", get(index))
         .route("/:id", get(redirect))
         .route("/", post(shorten))
         .with_state(state);
